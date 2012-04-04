9c9b2a63e377d5131385418d44b2fd1d1f0e7741
Jason Hsu <github@jasonhsu.com>
2012-04-04 14:47:13 -0500

    Added notes on how much disk space is freed

--- main.py
+++ main.py
@@ -32,46 +32,48 @@ def purge_packages (packages):
     os.system ('echo PURGING ' + packages)
     os.system ('apt-get purge -qq ' + packages)
 
-# Add gcj-jre (41.4 MB) as a lighter replacement for sun-java6-jre and sun-java6-bin (41.4MB).
-#os.system('apt-get install -y gcj-jre')
-#purge_packages ('gcj-jre')
+def package_add (name):
+    os.system ('echo ADDING ' + name)
+    os.system ('apt-get install -qq ' + name)
 
-# Now that gcj-jre is installed, sun-java6-jre and sun-java6-jre are no longer needed.
-# Remove sun-java6-jre and sun-java6-bin.
-#os.system('apt-get purge -y sun-java6-jre sun-java6-bin')
-#purge_packages ('sun-java6-jre sun-java6-bin')
+# Add gcj-jre as a lighter replacement for sun-java6-jre and sun-java6-bin (41.4MB).
+os.system ('echo Installing gcj-jre and dependent packages.')
+os.system ('echo This requires 12.0 MB of downloads and 41.4 MB of additional disk space.')
+package_add ('gcj-jre')
+
+# Now that gcj-jre is installed, sun-java6-jre and sun-java6-bin are no longer needed.
+os.system ('echo Purging sun-java6-jre and sun-java6-bin.')
+os.system ('echo This saves 104 MB of disk space.')
+purge_packages ('sun-java6-jre sun-java6-bin')
 
 # Removing selected LibreOffice packages
-#os.system('apt-get purge -y libreoffice libreoffice-emailmerge libreoffice-gnome python-uno')
-#purge_packages ('libreoffice libreoffice-emailmerge libreoffice-gnome python-uno')
+os.system ('echo Purging libreoffice libreoffice-emailmerge libreoffice-gnome python-uno')
+os.system ('echo This saves 2449 kB of disk space.')
+purge_packages ('libreoffice libreoffice-emailmerge libreoffice-gnome python-uno')
 
 # Remove LibreOffice Base
-#os.system('apt-get purge -y libreoffice-base libreoffice-report-builder-bin')
-#purge_packages ('libreoffice-base libreoffice-report-builder-bin')
+os.system ('echo Purging libreoffice-base libreoffice-report-builder-bin')
+purge_packages ('libreoffice-base libreoffice-report-builder-bin')
 
 # Remove LibreOffice Draw and Impress
-#os.system('apt-get purge -y libreoffice-draw libreoffice-impress')
-#purge_packages ('libreoffice-draw libreoffice-impress')
+os.system ('echo Purging LibreOffice Draw and Impress')
+os.system ('echo This saves 11.4 MB of disk space.')
+purge_packages ('libreoffice-draw libreoffice-impress')
 
 # Remove libreoffice-filter-mobiledev and libreoffice-java-common
-#os.system('apt-get purge -y libreoffice-filter-mobiledev libreoffice-java-common')
-#purge_packages ('libreoffice-filter-mobiledev libreoffice-java-common')
+os.system ('echo Purging libreoffice-filter-mobiledev libreoffice-java-common')
+os.system ('echo This saves 9126 kB of disk space.')
+purge_packages ('libreoffice-filter-mobiledev libreoffice-java-common')
 
 # Remove libreoffice-gtk
-#os.system('apt-get purge -y libreoffice-gtk')
-#purge_packages ('libreoffice-gtk')
+os.system ('echo Purging libreoffice-gtk')
+os.system ('echo This saves 1012 kB of disk space.')
+purge_packages ('libreoffice-gtk')
 
 # Remove LibreOffice Math
-#os.system('apt-get purge -y libreoffice-math')
-#purge_packages ('libreoffice-math')
-
-
-
-
-
-      
-      
-
+os.system ('echo Purging LibreOffice Math')
+os.system ('echo This saves 1540 kB of disk space.')
+purge_packages ('libreoffice-math')      
 
 os.system ("echo FINISHED SWITCHING TO LIGHTER JAVA PACKAGES")
 os.system ("echo ===========================================")
